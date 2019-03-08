b3abac59de1a2d2faf30c2e5943eb5d3059b0b31
Dennis Duong <dennis-duong@users.noreply.github.com>
2019-03-08 02:28:28 -0700

    adding core features
    chat history, online user list, username color and bold

--- index.js
+++ index.js
@@ -2,32 +2,44 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var date = new Date();
+var nicknameList = ['John', 'Bob', 'Robert', 'Jane', 'Eve', 'Alice'];
+var usedNicknames = [];
+
+var chatLogs = [];
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html'); //serves the html file to client
 });
 
 io.on('connection', function(socket){ //listen for connection event
-	var nickname = "Bob";
 
-	var info = new Object();
-	info.nickname = getNickName();
-	info.colour = getColour();
+	var user = new Object();
+	user.nickname = getNickname();
+	user.color = "red";
 
-	io.emit('connection message', nickname + " has connected.")
-	console.log('a user connected');
+	socket.emit('chat history', chatLogs); //sending chat history array to client
+
+	var connectionMsg = `<b><span style="color: ${user.color};">${user.nickname}</span></b> has connected.`;
+	io.emit('connection message', connectionMsg); //
+	io.emit('online users', usedNicknames);
+	chatLogs.push(connectionMsg);
+	console.log(chatLogs);
 
 	socket.on('disconnect', function(){ //listen for disconnect event
-		io.emit('connection message', nickname + " has disconnected.")
-		console.log('user disconnected');
+		var disconnectionMsg = `<b><span style="color: ${user.color};">${user.nickname}</span></b> has disconnected.`;
+		io.emit('connection message', disconnectionMsg);
+		chatLogs.push(disconnectionMsg);
+		console.log(chatLogs);
+		returnNickname(user.nickname);
+		io.emit('online users', usedNicknames);
 	});
 
 	socket.on('chat message', function(msg){
-		var currentTime = getTime();
+		user.msg = msg;
+		chatLogs.push(formatMsg(user));
+		io.emit('chat message', formatMsg(user));
+		console.log(chatLogs);
 
-		io.emit('chat message', currentTime + " " + nickname + ": " + msg); //send message to all clients
-		console.log('message: ' + currentTime + " " + nickname + ": " + msg);
 	});
 });
 
@@ -36,6 +48,7 @@ http.listen(3000, function(){
 });
 
 function getTime() {
+	var date = new Date();
 	var currentHour = date.getHours() < 10 ? '0'+ date.getHours() : date.getHours();
 	var currentMinute = date.getMinutes() < 10 ? '0'+ date.getMinutes() : date.getMinutes(); //formatting
 	var currentTime = currentHour + ":" + currentMinute;
@@ -43,6 +56,21 @@ function getTime() {
 }
 
 function getNickname() {
-	var names = []
+	var nickname = "defaultName";
+	if (nicknameList.length>0) {
+		nickname = nicknameList.shift();
+		usedNicknames.push(nickname);
+	} 
+	return nickname;
+}
 
+function returnNickname(nickname) {
+	nicknameList.push(nickname);
+	var pos = usedNicknames.indexOf(nickname);
+	usedNicknames.splice(pos, 1);
+}
+
+function formatMsg(user) {
+	var msg = `${getTime()} <b><span style="color: ${user.color};">${user.nickname}</span></b>: ${user.msg}`;
+	return msg;
 }
